db575f1a22c545623d94dafcbc373daa4fb5a2df
Nicolas Gomez <nicolasgomez94@gmail.com>
2025-11-13 14:29:10 -0300

    fix error import react

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { inspeccionesApi, GetAllParams } from '../api/inspecciones';
 import { InspeccionResponse } from '../types/inspeccion';
 import '../styles/Admin.css';
